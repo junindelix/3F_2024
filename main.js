130e7894762fbb99ed928b87c5c67e77cbd91cb1
SpessatoJr <108934932+junindelix@users.noreply.github.com>
2024-06-13 09:44:02 -0300

    13/06/24

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
-const abaconteudo = document.querySelectorAll(".abaconteudo");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
 
-for (let i = 0 ; i < botoes.length ; i++){
-    botoes[i].onclick = function (){
-        for (let j = 0; j < botoes.length ; j++){
-           botoes[j].classList.remove("ativo")
-           abaconteudo[j].classList.remove("ativo")
-       } 
-       botoes[i].classList.add("ativo")
-       abaconteudo[i].classList.add('ativo')
+for(let i = 0 ; i < botoes.length; i++){
+    botoes[i].onclick = function(){
+     for (let j = 0; j< botoes.length ; j++){
+         botoes[j].classList.remove("ativo")
+         conteudo[j].classList.remove("ativo")
+     }   
+     botoes[i].classList.add("ativo")
+     conteudo[i].classList.add("ativo")
     }
 }
 
