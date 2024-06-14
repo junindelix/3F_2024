1e07dddeb35b208c2b36a2027558cfe17000307c
SpessatoJr <108934932+junindelix@users.noreply.github.com>
2024-06-14 08:57:32 -0300

    14

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
+contador[0].textContent="Olá"
+contador[1].textContent="Olá"
+contador[2].textContent="Olá"
+contador[3].textContent="Olá"
+const tempoObjetivo1= new Date(2027,5,9,17,49)
+const tempoObjetivo2= new Date(2027,10,18,14,29)
+const tempoObjetivo3= new Date(2025,3,10,10,30)
+const tempoObjetivo4= new Date(2024,10,5,17,20)
+
+
 
 for(let i = 0 ; i < botoes.length; i++){
     botoes[i].onclick = function(){
