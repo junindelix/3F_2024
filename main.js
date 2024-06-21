0335ecc3393dc52a0ee567d3f6666bebfbab7d71
SpessatoJr <108934932+junindelix@users.noreply.github.com>
2024-06-21 08:58:44 -0300

    aula 21 06

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
 let agora = new Date();
+let segundos
+let minutos
+let horas
+let dias 
+Math.floor()
+segundos = (tempoObjetivo1-agora)/1000;
+minutos = segundos/60;
+horas = minutos/60;
+dias = horas/24;
+
+contador[0].textContent = `faltam 4{dias} dias, 7{horas} horas, 30{minutos} minutos e 48{segundos} segundos`
+
 const tempoObjetivo1= new Date(2027,5,9,17,49)
 const tempoObjetivo2= new Date(2027,10,18,14,29)
 const tempoObjetivo3= new Date(2025,3,10,10,30)
