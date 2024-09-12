c1f3400b93496b313324be16ae3f7b6b78c14f94
SpessatoJr <108934932+junindelix@users.noreply.github.com>
2024-09-12 09:44:20 -0300

    aula 12/09

--- main.js
+++ main.js
@@ -35,20 +35,19 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias,horas,minutos,segundos];
     } else {
         return "Prazo Finalizado";
     }
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[0](0))
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0](1))
-    document.getElementById("minutos0").textContent = calculaTempo(tempos[0](2))
-    document.getElementById("segundos0").textContent = calculaTempo(tempos[0](3))
-   
     for (let i=0; i<contadores.length;i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);  
+    document.getElementById('Dias'+i).textContent = calculaTempo(tempos[i])[0]
+    document.getElementById('Horas'+i).textContent = calculaTempo(tempos[i])[1]
+    document.getElementById('Minutos'+i).textContent = calculaTempo(tempos[i])[2]
+    document.getElementById('Segundos'+i).textContent = calculaTempo(tempos[i])[3]  
+         
     }
 }
 
